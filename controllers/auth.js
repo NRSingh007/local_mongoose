8aef5e96a54de96a4b37217aceeae4a24d38a20e
NRSingh007 <naorem.sana@gmail.com>
2020-09-08 08:55:46 +0530

    before hashing the password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,26 @@
-const User = require("../models/User")
+const User = require("../models/User");
+
+// handle errors
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: "", password: "" }; // creating an error objects
+
+  // duplicate error code
+  if (err.code == 11000) {
+    errors.email = "Email already exist, Try Sign In!!";
+    return errors;
+  }
+
+  // validations errors
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(properties);
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+};
 
 module.exports.signup_get = (req, res) => {
   res.render("signup");
@@ -8,14 +30,22 @@ module.exports.login_get = (req, res) => {
   res.render("login");
 };
 
-module.exports.signup_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-  } catch (err) {}
+    // create an instance of a user locally for us and the saves it to the database, so we need to pass in the object that defines the user
+    // this is an asynchronous task and it returns a promise
+    const newUser = await User.create({ email, password });
+    res.status(201).json(newUser);
+    console.log(newUser);
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
 };
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
   console.log(email, password);
   res.send("user login");
--- models/User.js
+++ models/User.js
@@ -1,20 +1,38 @@
 const mongoose = require("mongoose");
+const { default: validator } = require("validator");
+// importing third party validator
+const { isEmail } = require("validator");
 
 // creating userSchema
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Email is required"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Invalid Email!!, Please enter a valid Email address"],
   },
   password: {
     type: String,
-    required: true,
-    minlength: 8,
+    required: [true, "Password is required"],
+    minlength: [8, "Password should be minimum of 8 characters"],
   },
 });
 
+// fire a function after doc saved to db
+userSchema.post("save", function (doc, next) {
+  console.log("New user has been created and saved", doc);
+  next();
+});
+
+// fire a function before a doc was saved to db
+userSchema.pre("save", function (next) {
+  console.log("User is about to be created and saved", this);   // "this" refer to the local instance of the user before we saved it to the database
+  next();
+});
+
+// creating model based on the userSchema
+// "user" is the call model that define the database collection
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
